94cc0613efad907fea38b773472c0c1b7703d30f
sergio-rosendo-code <144377912+sergio-rosendo-code@users.noreply.github.com>
2024-03-05 16:40:20 -0800

    Added Swagger and OpenAPI support

--- APIs/FileSystem/API/Program.cs
+++ APIs/FileSystem/API/Program.cs
@@ -1,7 +1,26 @@
 using API.Infrastructure.Endpoints;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Title = "File System API",
+        Version = "V1"
+    });
+});
 
 var app = builder.Build();
 app.MapEndpoints();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(config =>
+    {
+        config.RoutePrefix = string.Empty;
+        config.SwaggerEndpoint("swagger/V1/swagger.json", "File System API V1");
+    });
+}
 app.Run();
